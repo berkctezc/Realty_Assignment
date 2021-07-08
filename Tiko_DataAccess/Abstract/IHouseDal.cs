65f9d6b6c5494d03f6c1143072651e2e98ab5e4a
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-08 19:11:58 +0300

    added and implemented house price update method

--- Tiko_DataAccess/Abstract/IHouseDal.cs
+++ Tiko_DataAccess/Abstract/IHouseDal.cs
@@ -1,9 +1,10 @@
-﻿using Tiko_Entities.Concrete;
+﻿using System.Threading.Tasks;
+using Tiko_Entities.Concrete;
 
 namespace Tiko_DataAccess.Abstract
 {
     public interface IHouseDal:IRepository<House>
     {
-        
+        Task UpdateHousePriceAsync(House house, int newPrice);
     }
 }
--- Tiko_DataAccess/Concrete/EfDal.cs
+++ Tiko_DataAccess/Concrete/EfDal.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Tiko_DataAccess.Abstract;
 using Tiko_Entities.Concrete;
+using Tiko_WebAPI.Data;
 
 namespace Tiko_DataAccess.Concrete
 {
     public class EfAgentDal : GenericRepositoryEf<Agent>, IAgentDal { }
     public class EfCityDal : GenericRepositoryEf<City>, ICityDal { }
-    public class EfHouseDal : GenericRepositoryEf<House>, IHouseDal { }
+    public class EfHouseDal : GenericRepositoryEf<House>, IHouseDal
+    {
+        private readonly TikoDbContext _context = new();
+
+        public EfHouseDal(TikoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task UpdateHousePriceAsync(House house, int newPrice)
+        {
+            House houseToUpdate = await _context.Houses.SingleAsync(x => x.Id == house.Id);
+            houseToUpdate.Price = newPrice;
+            await _context.SaveChangesAsync();
+        }
+    }
 }
